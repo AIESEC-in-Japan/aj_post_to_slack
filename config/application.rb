d511e4ebf1256d4e082504c712cac94d3b3267b0
yuki komatsu <yuki.k.lion@gmail.com>
2019-03-01 12:43:27 +0900

    conf: do not create useless files

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module AjPostToSlack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+     config.generators do |g|
+      g.assets false
+      g.helper false
+     # g.jbuilder false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
